caa6e73f67696bb70ff8db464b6b1985f67f7ced
Brandon Block <branblock@gmail.com>
2015-10-04 20:49:24 -0700

    Assignment-25 revised

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -46,7 +46,18 @@ RSpec.describe AddressBook do
       expect(book.entries.size).to eq(0)
     end
   end
-  
+
+  describe "#detonate" do
+    it "should remove all entries" do
+      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      
+      book.detonate
+      expect(book.entries.size).to eq(0)
+    end
+  end
+    
   describe "#import_from_csv" do
     it "imports the correct number of entries" do
       book.import_from_csv("entries.csv")
